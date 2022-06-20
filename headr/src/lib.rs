56f0a69bff3e750a1e2b0dd5c5fc3f2a5b623bc7
Blake Hachen <blakehachen@ksu.edu>
2022-06-20 01:29:17 -0500

    headr tests passing

---
+++ headr/src/lib.rs
@@ -0,0 +1,115 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>>{
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
+}
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Blake Hachen <blakehachen@ksu.edu>")
+        .about("head for rust")
+        .arg(
+            Arg::with_name("filename")
+                .value_name("FILE")
+                .help("name of file(s) to display")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .value_name("LINES")
+                .short("n")
+                .long("lines")
+                .help("display the top n lines of file (default: n = 10)")
+                .takes_value(true)
+                .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .value_name("BYTES")
+                .short("c")
+                .conflicts_with("lines")
+                .long("bytes")
+                .help("display first n bytes of file")
+                .takes_value(true),
+        )
+        .get_matches();
+    
+    let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+            
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+    
+    Ok(Config {
+        files: matches.values_of_lossy("filename").unwrap(),
+        lines: lines.unwrap(),
+        bytes
+    })
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize>{
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(val.into())
+    }
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let num_files = config.files.len();
+
+    for (file_num, filename) in config.files.iter().enumerate() {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file) => {
+                if num_files > 1 {
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 {"\n"} else {""},
+                        filename
+                    );
+                }
+
+                if let Some(num_bytes) = config.bytes {
+                    let mut handle = file.take(num_bytes as u64);
+                    let mut buffer = vec![0; num_bytes];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!(
+                        "{}",
+                        String::from_utf8_lossy(&buffer[0..bytes_read])
+                    );
+                }else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear();
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
